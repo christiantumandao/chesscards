41c4a12a22f237257bee732f4e41d67c61506627
christiantumandao <christianptumandao@gmail.com>
2026-01-07 21:08:56 -0500

    Fixed incorrect/correct buzzer on freestyle bug

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -139,9 +139,10 @@ const Game = ({ makeAMove, lastSquare, setLastSquare, lastMove, playSound }: Gam
 
         } else {
             triggerIncorrectAnimation();
+            playSound("incorrect");
             setTimeout(()=>{
                 incrementIncorrects();
-                playSound("incorrect");
+                
                 setPlayerMoveIdx(0);
                 setGame(new Chess());
                 setCurrTrie(trieHead);
